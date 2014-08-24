69c9b7e0c23c52d2e8db68c50a86d003b897f6d3
David Marx <david.marx84@gmail.com>
2014-08-24 12:58:53 -0400

    Moved bomb-specific functions out of buildgraph.js and into own module (bombs.js). Renamed mines.js to game.js for clarity.

--- buildgraph.js
+++ buildgraph.js
@@ -82,25 +82,3 @@ var buildGraph = function(){
     network.svg = svg;
     return network;
 }
-
-var setBombsSVG = function(network){
-    network.node.classed("bomb", function(d){return d.bomb})
-}
-
-var countBombs = function(d){
-    var neighbors = graph.adjacency[d.id];
-        var neighbor_bombs = 0;
-        for( id in neighbors ){
-            if($('.node.bomb#' + id).length > 0) ++neighbor_bombs;
-        }
-        console.log(d.id);
-    console.log(neighbors);
-    console.log(neighbor_bombs);
-    return neighbor_bombs;
-};
-
-var calcBombDegree = function(network){
-    network.node.each(function(d) {
-        this.setAttribute("bombDegree", countBombs(d));
-    });
-}
