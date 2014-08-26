807451966cd0cc0e0cc29630da800f18b403b8cb
davidmarx <david.marx@datamininglab.com>
2014-08-26 15:16:49 -0400

    Better (centered) flag counter formatting

--- stats.js
+++ stats.js
@@ -1,7 +1,7 @@
-var flag_counter = d3.select("#flag_counter").data(n_flags);
+var flag_counter = d3.select("#flag-counter-value").data(n_flags);
 flag_counter.append("p").text(n_flags); //id setter not working properly.
 
 function updateFlagsCount(){
-    d3.select("#flag_counter>p:not(#flag-count-label)").remove()
+    d3.select("#flag-counter-value>p").remove()
     flag_counter.append("p").text(n_flags);
     };
