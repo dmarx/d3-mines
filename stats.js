d6e30eefd0ffe20f05cba2bbce7842f8a277dd92
davidmarx <david.marx@datamininglab.com>
2014-09-05 11:35:23 -0400

    Flask is successfully serving the app.

--- stats.js
+++
@@ -1,7 +0,0 @@
-var flag_counter = d3.select("#flag-counter-value").data(n_flags);
-flag_counter.append("p").text(n_flags); //id setter not working properly.
-
-function updateFlagsCount(){
-    d3.select("#flag-counter-value>p").remove()
-    flag_counter.append("p").text(n_flags);
-    };
