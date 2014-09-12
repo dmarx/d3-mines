33f1e5660a11bd6cf92181c9e7304517dd2b7d20
davidmarx <david.marx@datamininglab.com>
2014-09-12 15:43:22 -0400

    Forgot to add idle.js

---
+++ static/js/idle.js
@@ -0,0 +1,29 @@
+idleTimer = null;
+idleState = true;
+idleWait = 2000;
+
+$("#instructions").hide();
+
+(function ($) {
+    $(document).ready(function () {
+        $('*').bind('mousemove keydown scroll', function () {
+            clearTimeout(idleTimer);
+            if (idleState == false) { 
+                // Reactivated event
+                //$("body").append("<p>Welcome Back.</p>");            
+                $("#instructions").hide();
+            }
+            
+            idleState = true;
+            idleTimer = setTimeout(function () { 
+                
+                // Idle Event
+                if(idleState == true && !timerOn){
+                    $("#instructions").show();
+                    idleState = false; 
+                }
+                }, idleWait);
+        });
+        $("body").trigger("mousemove");
+    });
+}) (jQuery)
