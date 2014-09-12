d11abdff91a1e9e3a5bee86d78a4601dcc4d4069
davidmarx <david.marx@datamininglab.com>
2014-09-12 15:13:04 -0400

    Prettied up the win animation a bit

--- static/js/momentumFriction.js
+++ static/js/momentumFriction.js
@@ -3,7 +3,7 @@ function winAnimation(){
     var anim = d3.layout.force()
             .gravity(0)
             //.charge(0)
-            .charge(-60)
+            .charge(-40)
             .friction(0.95);
 
     var svg2 = d3.select("#animation").append("svg:svg");
@@ -22,21 +22,36 @@ function winAnimation(){
                 x: point[0],
                 y: point[1],
                 px: previousPoint?previousPoint[0]:point[0],
-                py: previousPoint?previousPoint[1]:point[1]
+                py: previousPoint?previousPoint[1]:point[1],
+                col: randInt(10)
             };
 
         previousPoint = point;
-
+        
+        var color = d3.scale.category10().domain([0,1,2,3,4,5,6,7,8,9]);
+        
+        var sustain = 1000;
         svg2.append("svg:circle")
                     .data([node])
                 .attr("class", "node")
                 .attr("cx", function (d) {return d.x;})
                 .attr("cy", function (d) {return d.y;})
-                .attr("r", 1e-6)
+                .style("fill", function(d) {console.log(d.col); return color(d.col); })
+                .style("stroke", function(d) { return color(d.col); })
+                .attr("r", 1e-5)
+                //.attr("opacity",1)
             .transition()
-                .attr("r", 4.5)
+                //.attr("r", 4.5)
+                .attr("r", randInt(12))
+                .attr("opacity",.6)
+                .duration(sustain/2)
+            //.transition()
+                //.duration(2*sustain)
+                //.ease()
+                //.delay(sustain)
             .transition()
-            .delay(5000)
+                //.delay(sustain)
+                .attr("opacity",.1)
                 .attr("r", 1e-6)
                 .each("end", function () {
                     anim.nodes().shift();
